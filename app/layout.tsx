e34e697508243f2bc14fbd94a3395c949a39f4eb
Wilberk <ledezma.wilberk@gmail.com>
2025-05-04 23:32:41 -0400

    update link text size in navigation for improved responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"} className="text-2xl">
+                    <Link href={"/"} className="text-md lg:text-lg xl:text-2xl">
                       Portfolio Manager
                     </Link>
                   </div>
